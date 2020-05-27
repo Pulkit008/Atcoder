a82cfaa2dece1144bf809e726d9f630df5e92f0d
Pulkit008 <61957883+Pulkit008@users.noreply.github.com>
2020-05-27 20:34:18 +0530

    Update I-Coins.py

--- Educational DP Contest/I-Coins.py
+++ Educational DP Contest/I-Coins.py
@@ -8,3 +8,19 @@ for i in range(1,n+1):
         else:
             dp[i][j]=dp[i-1][j]*(1.0-l[i-1])+dp[i-1][j-1]*l[i-1]
 print(sum(dp[n][((n+1)//2):]))
+
+"""
+from numpy import zeros
+ 
+N = int(input())
+P = map(float, input().split())
+ 
+dp = zeros(N + 1)
+dp[0] = 1.0
+ 
+for p in P:
+    T = dp
+    dp = T * (1 - p)
+    dp[1:] += T[:-1] * p
+ 
+print(dp[-(-N // 2):].sum())"""
